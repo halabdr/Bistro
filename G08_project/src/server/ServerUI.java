aa68617c76d82855117bd3472e45deaec9500002
AyaShali <User@10.10.5.145>
2025-12-05 20:17:09 +0200

    I added a comment

--- G08_project/src/server/ServerUI.java
+++ G08_project/src/server/ServerUI.java
@@ -2,6 +2,7 @@ package server;
 
 public class ServerUI {
 
+	//main for serverUI
     public static void main(String[] args) {
         int port = 5555; // כמו אצלך קודם
         EchoServer server = new EchoServer(port);
