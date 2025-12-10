c36de937956eeee5e3c3bca81cc5cd9451c3b4aa
Johara Naser Aldeen <johara2012000@gmail.com>
2025-12-10 15:57:37 +0200

    Delete פרוייקט אמצע/Assignment 2/running_server.bat

--- פרוייקט אמצע/Assignment 2/running_server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java --module-path "C:\Users\User\OneDrive - ort braude college of engineering\Documents\javafx-sdk-21.0.9\lib" --add-modules javafx.controls,javafx.fxml -jar G08_Prototype_Server.jar 2>nul
-pause
