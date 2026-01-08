522f7ea43c184bcf1093d8d0ab00dba99812e76e
Johara <johara2012000@gmail.com>
2026-01-08 23:03:25 +0200

    change common package to keep it minimal

--- Assignment3/BistroCommon/src/common/Message.java
+++ Assignment3/BistroCommon/src/common/Message.java
@@ -4,23 +4,21 @@ import java.io.Serializable;
 
 /**
  * Represents a message that is sent between the client and the server.
- * Each message contains a command type and optional data.
+ * Each message contains a command string and optional data.
  * It can also represent a successful response or an error.
  */
 public class Message implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private final CommandType command;
+    private final String command;
     private final Object data;
-
     private final boolean success;
     private final String error;
 
     /**
      * Creates a new message.
      */
-    private Message(CommandType command, Object data, boolean success, String error) 
-    {
+    private Message(String command, Object data, boolean success, String error) {
         this.command = command;
         this.data = data;
         this.success = success;
@@ -30,67 +28,78 @@ public class Message implements Serializable {
     /**
      * Creates a successful message with data.
      *
-     * @param command the command type
+     * @param command the command string
      * @param data the data to send
      */
-    public Message(CommandType command, Object data) 
-    {
+    public Message(String command, Object data) {
         this(command, data, true, null);
     }
 
     /**
      * Creates a successful response message.
      *
-     * @param command the command type
+     * @param command the command string
      * @param data the returned data
      * @return a success message
      */
-    public static Message ok(CommandType command, Object data) 
-    {
+    public static Message ok(String command, Object data) {
         return new Message(command, data, true, null);
     }
     
     /**
      * Creates a failure response message.
      *
-     * @param command the command type
+     * @param command the command string
      * @param error error description
      * @return a failure message
      */
-    public static Message fail(CommandType command, String error) 
-    {
+    public static Message fail(String command, String error) {
         return new Message(command, null, false, error);
     }
    
     /**
-     * @return the command type
+     * Gets the command string.
+     * 
+     * @return the command string
      */
-    public CommandType getCommand() 
-    { 
-    	    return command; 
+    public String getCommand() {
+        return command;
     }
     
     /**
+     * Gets the message data.
+     * 
      * @return the message data
      */
-    public Object getData() 
-    { 
-    	    return data; 
-    	}
+    public Object getData() {
+        return data;
+    }
     
     /**
+     * Checks if the operation succeeded.
+     * 
      * @return true if the operation succeeded
      */
-    public boolean isSuccess() 
-    { 
-    	    return success; 
-    	}
+    public boolean isSuccess() {
+        return success;
+    }
     
     /**
+     * Gets the error message if failed.
+     * 
      * @return error message if failed, null otherwise
      */
-    public String getError() 
-    { 
-    	    return error; 
-    	}
+    public String getError() {
+        return error;
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "command='" + command + '\'' +
+                ", success=" + success +
+                ", error='" + error + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
